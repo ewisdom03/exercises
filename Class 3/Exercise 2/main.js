d310bb4463007c2f0974a4408ca49de5c236b6c7
Ethan Wisdom <ethanwisdom03@gmail.com>
2022-03-14 17:38:40 -0500

    EDIT: Work on Exercises for classes 2, 3, 4, and 8

--- Class 3/Exercise 2/main.js
+++ Class 3/Exercise 2/main.js
@@ -1,5 +1,12 @@
-const user = {
-    name: "Ethan",
-    age: 18,
-    occupation: "none",
+function employee(){
+    const user = {
+        name: "Ethan",
+        age: 18,
+        occupation: "none",
+    }
+
+    console.log(user)
 }
+
+enter.addEventListener("click", employee)
+
