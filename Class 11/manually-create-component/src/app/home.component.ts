3d05641a13a5f1e473996279b7b17cba80093f39
Ethan Wisdom <ethanwisdom03@gmail.com>
2022-03-28 19:27:06 -0500

    ADDED: Classes 11 and 13

---
+++ Class 11/manually-create-component/src/app/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { ComponentFixtureAutoDetect } from "@angular/core/testing";
+
+@Component({
+  selector: "home-component",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
+})
+
+export class HomeComponent{
+
+}
